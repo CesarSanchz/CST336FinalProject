69b513d9f1179f127b2e47d838d7dcda3d237829
CesarSanchz <sanchzcesr@gmail.com>
2020-08-13 05:03:32 +0000

    Changing product page visuals

--- public/js/catalog.js
+++ public/js/catalog.js
@@ -19,21 +19,34 @@ $(document).ready(function() {
       },
       success: function(response) {
         // Validation for input field
+        
+        $("#productPage").html(
+          "<table id='tableInProduct'>"+
+               "<tr>" +
+                   "<th>ID</th>"+
+                   "<th>Picture</th>" +
+                   "<th>Name</th>"+
+                   "<th>Model</th>"+
+                   "<th>Manufacturer</th>"+
+                   "<th>Component</th>"+
+                   "<th>Price</th>"+
+                   "<th>Button</th>"+
+               "</tr>"
+        );
         for (var i = 0; i < response.length; i++) {
-          $("#productPage").append(
+          $("#tableInProduct").append(
             "<tr class='tableInfo' value='" + response[i].id + "'>" +
-            " <td id='productID'>" + response[i].id + "</td> " +
-            " <td> <img id='productImage' alt='" + response[i].pictureURL + "' src='" + response[i].pictureURL + "' width='200' height='200'></td>" +
-            " <td id='productTitle'>" + response[i].make + "</td> " +
-            " <td>" + response[i].model + "</td> " +
-            " <td>" + response[i].manufacturer + "</td> " +
-            " <td>" + response[i].type + "</td> " +
-            " <td id='productPrice' >" + response[i].price + "</td> " +
+            "<td id='productID'>" + response[i].id + "</td> " +
+            "<td> <img id='productImage' alt='" + response[i].pictureURL + "' src='" + response[i].pictureURL + "' width='200' height='200'></td>" +
+            "<td id='productTitle'>" + response[i].make + "</td> " +
+            "<td>" + response[i].model + "</td> " +
+            "<td>" + response[i].manufacturer + "</td> " +
+            "<td>" + response[i].type + "</td> " +
+            "<td id='productPrice' >" + response[i].price + "</td> " +
             "<td><button class ='addCart' value ='" + response[i].id + "'> Add to Cart </td>" +
             "</tr>"
           );
         }
-
         //$("#displayTable").html("results for " + response[0].make);
       }
 
